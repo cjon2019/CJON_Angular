3bea4a5e967cb112d1a6883576fc586765ae34b9
Owen Edwards <omedwards@outlook.com>
2019-04-17 16:40:43 -0400

    finishing bug touch ups, maybe

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -17,7 +17,7 @@ export class AuthService {
   constructor(private _http: HttpClient, private _router: Router) { }
 
   register(regUserData: RegisterUser) {
-    return this._http.post(`${Api_Url}/api/v1/Register`, regUserData);
+    return this._http.post(`${Api_Url}/api/v1/users`, regUserData);
   }
 
   login(loginInfo) {
