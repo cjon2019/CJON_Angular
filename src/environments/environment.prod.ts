a7b26468ffe189c89f1758435ef3a738e5ef1f20
Owen Edwards <omedwards@outlook.com>
2019-04-24 17:29:57 -0400

    fixed api url link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export let APIURL = 'https://cjon-red-badge-angular-project.herokuapp.com';
+export let APIURL = 'https://cjon-red-badge-project.herokuapp.com';
